4440bf08e67d6918d71d8b37071aed5239933f1e
Vivian stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-07-24 17:36:33 -0700

    commit tree 76a7492d9b66152c5e161371a24efd8cf8b51c6b

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,42 +4,38 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    # all.limit(5)
+    self.all.limit(5)
   end
 
   def self.dinghy
-    # where("length < 20")
+    self.where("length < ?", 20)
   end
 
   def self.ship
-    # where("length >= 20")
+    self.where("length >= ?", 20)
   end
 
   def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+    self.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
-    # where(captain_id: nil)
+    self.where(captain: nil)
   end
 
   def self.sailboats
-    # includes(:classifications).where(classifications: { name: 'Sailboat' })
+    includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
-    # This is really complex! It's not common to write code like this
-    # regularly. Just know that we can get this out of the database in
-    # milliseconds whereas it would take whole seconds for Ruby to do the same.
-    #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    self.joins(:boat_classifications).group('boats.id').having('count(boat_id) = 3')
   end
 
-  def self.non_sailboats
-    # where("id NOT IN (?)", self.sailboats.pluck(:id))
+  def self.max_length
+    self.maximum("length")
   end
 
   def self.longest
-    # order('length DESC').first
+    self.order('length DESC').first
   end
 end
